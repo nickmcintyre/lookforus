478c09ebf0d94466b8206896f9e6e94bb12bcac8
Nick McIntyre <nick@mcintyre.io>
2019-11-23 15:50:38 -0600

    Update dependencies

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import {
   Button,
-  Container,
+  Grid,
   Header,
   Image,
   Segment,
@@ -32,32 +32,34 @@ class App extends Component {
 
   renderInvitation() {
     return (
-      <Container text>
-        <Image src={'/images/lookforus.png'} size="medium" centered></Image>
-        <Header as="h2">Welcome to the network.</Header>
-        <Segment>
-          <Header as="h3"><FaUserSecret /> A note on privacy</Header>
-          <p>
-            Video is never transferred from your device. The small amount of
-            data needed to represent you is completely anonymous and deleted
-            every 5 seconds. You are welcome to view the
-            app's <a href="https://github.com/nickmcintyre/lookforus" target="_blank">
-            source code</a> or <a href="mailto:nick@mcintyre.io?Subject=Look%20for%20us">
-            email me</a> with any questions.
-          </p>
-        </Segment>
-        <Button
-          onClick={() => this.setState({ participating: true })}
-          style={{ width: '100%' }}
-          primary
-        >
-          Participate
-        </Button>
-        <p style={{ textAlign: 'center', fontSize: '1rem', paddingTop: '2em', paddingBottom: '2em' }}>
-          Copyright &copy; 2019 Nick McIntyre. <a href="https://github.com/nickmcintyre/lookforus/blob/master/LICENSE">
-          MIT License</a>.
-        </p>
-      </Container>
+        <Grid stackable centered columns={3}>
+          <Grid.Column>
+            <Image src={'/images/lookforus.png'} size="medium" centered></Image>
+            <Header as="h2">Welcome to the network.</Header>
+            <Segment>
+              <Header as="h3"><FaUserSecret /> A note on privacy</Header>
+              <p>
+                Video is never transferred from your device. The small amount of
+                data needed to represent you is completely anonymous and deleted
+                every 5 seconds. You are welcome to view the
+                app's <a href="https://github.com/nickmcintyre/lookforus" target="_blank">
+                source code</a> or <a href="mailto:nick@mcintyre.io?Subject=Look%20for%20us">
+                email me</a> with any questions.
+              </p>
+            </Segment>
+            <Button
+              onClick={() => this.setState({ participating: true })}
+              style={{ width: '100%' }}
+              primary
+            >
+              Participate
+            </Button>
+            <p style={{ textAlign: 'center', fontSize: '1rem', paddingTop: '2em', paddingBottom: '2em' }}>
+              Copyright &copy; 2019 Nick McIntyre. <a href="https://github.com/nickmcintyre/lookforus/blob/master/LICENSE">
+              MIT License</a>.
+            </p>
+          </Grid.Column>
+        </Grid>
     );
   }
 
