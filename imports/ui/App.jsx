397c3ed38c378ce702b25cbe5bb68b3e9355f3cc
Nick McIntyre <nick@mcintyre.io>
2019-11-25 09:52:10 -0600

    Play with cards

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import {
   Button,
+  Card,
   Grid,
   Header,
+  Icon,
   Image,
   Segment,
 } from 'semantic-ui-react';
-import { FaUserSecret} from 'react-icons/fa';
 
 import Sketch from './Sketch';
 
@@ -32,48 +33,60 @@ class App extends Component {
 
   renderInvitation() {
     return (
-        <Grid stackable centered columns={3}>
+      <Grid
+        verticalAlign='middle'
+        centered
+        columns={3}
+        stackable={true}
+        style={{ height: '100vh', width: '100%' }}
+      >
+        <Grid.Row>
           <Grid.Column>
-            <Image src={'/images/lookforus.png'} size="medium" centered></Image>
-            <Header as="h2">Welcome to the network.</Header>
-            <Segment>
-              <Header as="h3"><FaUserSecret /> A note on privacy</Header>
-              <p>
-                Video is never transferred from your device. The small amount of
-                data needed to represent you is completely anonymous and deleted
-                every 5 seconds. You are welcome to view the
-                app's <a href="https://github.com/nickmcintyre/lookforus" target="_blank">
-                source code</a> or <a href="mailto:nick@mcintyre.io?Subject=Look%20for%20us">
-                email me</a> with any questions.
-              </p>
-            </Segment>
-            <Button
-              onClick={() => this.setState({ participating: true })}
-              style={{ width: '100%' }}
-              primary
-            >
-              Participate
-            </Button>
-            <p style={{ textAlign: 'center', fontSize: '1rem', paddingTop: '2em', paddingBottom: '2em' }}>
-              Copyright &copy; 2019 Nick McIntyre. <a href="https://github.com/nickmcintyre/lookforus/blob/master/LICENSE">
-              MIT License</a>.
-            </p>
+            <Card fluid={true}>
+              <Image src={'/images/lookforus.png'} wrapped ui={false} />
+              <Card.Content style={{ borderTop: 'none' }}>
+                <Card.Header>Welcome to the network.</Card.Header>
+                <Card.Description>
+                  <Segment>
+                    <Header as='h3'><Icon name='user secret'/> A note on privacy</Header>
+                    <p>
+                      Video is never transferred from your device. The small amount of
+                      data needed to represent you is completely anonymous and deleted
+                      every 5 seconds. You are welcome to view the
+                      app's <a href="https://github.com/nickmcintyre/lookforus" target="_blank">
+                      source code</a> or <a href="mailto:nick@mcintyre.io?Subject=Look%20for%20us">
+                      email me</a> with any questions.
+                    </p>
+                  </Segment>
+                  <Button
+                    onClick={() => this.setState({ participating: true })}
+                    style={{ width: '100%' }}
+                    primary
+                  >
+                    Participate
+                  </Button>
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <p style={{ textAlign: 'center', fontSize: '1rem', paddingTop: '2em', paddingBottom: '2em' }}>
+                  Copyright &copy; 2019 Nick McIntyre. <a href="https://github.com/nickmcintyre/lookforus/blob/master/LICENSE">
+                  MIT License</a>.
+                </p>
+              </Card.Content>
+            </Card>
           </Grid.Column>
-        </Grid>
+        </Grid.Row>
+      </Grid>
     );
   }
 
   render() {
     const { participating } = this.state;
-
-    return (
-      <div>
-        {participating ?
-          <Sketch id={'canvas-container'} />
-          : this.renderInvitation()
-        }
-      </div>
-    );
+    if (participating) {
+      return <Sketch id={'canvas-container'} />;
+    } else {
+      return this.renderInvitation();
+    }
   }
 }
 
